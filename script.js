3d2f82176279107ab80fcf23b5e1efd2f8f1155e
Rafa Kraetzer <rafakraetzer@hotmail.com>
2021-07-10 18:07:30 -0300

    botoes
    botoes funcionando

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 let listaBomba = []
+let saida = document.getElementById("txtSaida")
 
-function inserirlistaBomba(){
-    let saida = document.getElementById("txtSaida")
-    
+function inserirReparo(){
+    let procura = listaBomba.indexOf("Reparo")
+    if(procura > -1){
+        listaBomba.splice(procura, 1)
+        mostrarSaida()
+    }else{
+        listaBomba.push("Reparo")
+        mostrarSaida()
+    }
+}
+function inserirEsfera(){
+    let procura = listaBomba.indexOf("Esfera")
+    if(procura > -1){
+        listaBomba.splice(procura, 1)
+        mostrarSaida()
+    }else{
+        listaBomba.push("Esfera")
+        mostrarSaida()
+    }
+}
 
-
-    saida.innerText = "listaBomba[0]"
+function mostrarSaida(){
+    saida.innerText = ""
+    for(let i = 0; i < listaBomba.length; i++){
+        saida.innerHTML += listaBomba[i] + '\n';
+    }
 }
