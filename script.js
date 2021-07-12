0b81f6383b9e96ac2ee6f58ea4cc41ad639a8d1c
Rafa Kraetzer <rafakraetzer@hotmail.com>
2021-07-12 20:14:31 -0300

    onInput
    funcao on input

--- script.js
+++ script.js
@@ -5,30 +5,30 @@ let saida = document.getElementById("txtSaida")
     verifica a existencia do item na lista,
     passado como parametro pelos botoes.
 */
-function busca(item){
+function busca(item) {
     let procura = listaBomba.indexOf(item)
-    if(procura > -1){
+    if (procura > -1) {
         listaBomba.splice(procura, 1)
         mostrarSaida()
-    }else{
+    } else {
         listaBomba.push(item)
         mostrarSaida()
     }
 }
 
 //botoes para inserir item na lista
-function inserirReparo(){
+function inserirReparo() {
     busca("01 Reparo Bomba Alta")
 }
-function inserirEsfera(){
+function inserirEsfera() {
     busca("03 Esferas")
 }
 
 /*
     recebe dados do cliente,
     valida campos nulos para saida.
-*/ 
-function dadosCliente(){
+*/
+function dadosCliente() {
     let txtCli = document.getElementById("txtCli")
     let txtCliente = txtCli.value.toUpperCase()
     let txtPla = document.getElementById("txtPla")
@@ -37,13 +37,14 @@ function dadosCliente(){
     let km = txtKm.value.toUpperCase()
     let txtVei = document.getElementById("txtVei")
     let txtVeiculo = txtVei.value.toUpperCase()
-    saida.innerHTML = `Cliente.: ${txtCliente}\nPlaca.: ${txtPlaca}\nKM.:${km}\nVeiculo.: ${txtVeiculo}\n`
+    
+    saida.innerHTML = `Cliente.: ${txtCliente}\nPlaca.: ${txtPlaca}\nKM.:${km}\nVeiculo.: ${txtVeiculo}\n────────────────────────────\n`
 }
 
 //mostra saida na area de texto
-function mostrarSaida(){
+function mostrarSaida() {
     dadosCliente()
-    for(let i = 0; i < listaBomba.length; i++){
+    for (let i = 0; i < listaBomba.length; i++) {
         saida.innerHTML += listaBomba[i] + '\n'
     }
 }
