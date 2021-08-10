220fa02ef055739600286cfcdc3c3097ae7abee0
Rafa Kraetzer <rafakraetzer@hotmail.com>
2021-08-09 21:19:29 -0300

    sem gray a mais

--- script.js
+++ script.js
@@ -40,12 +40,15 @@ function busca(qtd, item, botao) {
         if (procura > -1) { //se achar
             listaPeca.splice(procura, 1)
             botao.style.backgroundColor = "white"
+            botao.style.color = "black"
             mostrarSaida()
             break
         }
         ver += 1
     }
     if(ver == 7){
+        botao.style.backgroundColor = "#1D566B"
+        botao.style.color = "white"
         listaPeca.push(qtd + item)
         mostrarSaida()
     }      
@@ -56,8 +59,11 @@ function buscaServico(servico, botao){
     if(procura > -1){
         listaServico.splice(procura, 1)
         botao.style.backgroundColor = "white"
+        botao.style.color = "black"
         mostrarSaida()
     }else{
+        botao.style.backgroundColor = "#1D566B"
+        botao.style.color = "white"
         listaServico.push(servico)
         mostrarSaida()
     }
@@ -70,17 +76,14 @@ function limpar() {
 
 //botoes para inserir item na lista bba alta
 function inserirReparo(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Reparo Bomba Alta", botao)
 }
 function inserirEsfera(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Esfera", botao)
 }
 function inserirAspiracao(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Valvula Aspiracao", botao)
 }
@@ -88,11 +91,9 @@ function inserirMprop(botao) {
     if(contProp == 0){
         numProp = window.prompt("Informe o numero da M-prop:").toUpperCase()
         contProp = 1
-        botao.style.backgroundColor = "gray"
         radios(qtd)
         busca(qtd ," Valvula M-prop " + numProp, botao)
     }else{
-        botao.style.backgroundColor = "gray"
         radios(qtd)
         busca(qtd ," Valvula M-prop " + numProp, botao)
         contProp = 0
@@ -102,101 +103,83 @@ function inserirDrv(botao) {
     if(contDrv == 0){
         numDrv = window.prompt("Informe o numero da DRV:").toUpperCase()
         contDrv = 1
-        botao.style.backgroundColor = "gray"
         radios(qtd)
         busca(qtd ," Valvula DRV " + numDrv, botao)
     }else{
-        botao.style.backgroundColor = "gray"
         radios(qtd)
         busca(qtd ," Valvula DRV " + numDrv, botao)
         contDrv = 0
     }
 }
 function inserirMola(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Mola", botao)
 }
 function inserirRetentor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Retentor", botao)
 }
 function inserirEixo(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Eixo Acionamento", botao)
 }
 function inserirElemento(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Elemento", botao)
 }
 function inserirBaixa(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Bomba de Baixa", botao)
 }
 function inserirAlta(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Bomba de Alta", botao)
 }
 
 function inserirAnelFla(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Anel Flange Bba", botao)
 }
 
 //inserir lista servico
 function inserirRevBbaServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Revisao Bba Alta", botao)
 }
 
 function inserirTestBbaServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Teste Bba Alta", botao)
 }
 
 function inserirLimpBbaServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Limpeza Bba Alta", botao)
 }
 
 function inserirRetBbaServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Ret/Colocar Bba Alta", botao)
 }
 
 //botoes para inserir lista injetor
 function inserirReparoInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Reparo Injetor", botao)
 }
 
 function inserirEsferaInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Esfera Injetor", botao)
 }
 
 function inserirCalcoInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Calco Injetor", botao)
 }
 
 function inserirArruelaInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Arruela Injetor", botao)
 }
 
 function inserirConjValvula(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Conjunto Valvula Injetor", botao)
 }
@@ -205,11 +188,9 @@ function inserirBicoInjetor(botao) {
     if(contBico == 0){
         numBico = window.prompt("Informe o numero do Bico:").toUpperCase()
         contBico = 1
-        botao.style.backgroundColor = "gray"
         radios(qtd)
         busca(qtd ," Bico Injetor " + numBico, botao)
     }else{
-        botao.style.backgroundColor = "gray"
         radios(qtd)
         busca(qtd ," Bico Injetor " + numBico, botao)
         contBico = 0
@@ -217,118 +198,99 @@ function inserirBicoInjetor(botao) {
 }
 
 function inserirPorcaInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Porca Injetor", botao)
 }
 
 function inserirAnelInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Anel Injetor", botao)
 }
 
 function inserirFiltroInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Filtro Injetor", botao)
 }
 
 function inserirSolenoideInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Solenoide Injetor", botao)
 }
 
 function inserirAnelSolenoideInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Anel Solenoide Injetor", botao)
 }
 
 function inserirCompletoInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Injetor Completo", botao)
 }
 
 function inserirRecuperacaoInjetor(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd , " Recuperacao Injetor", botao)
 }
 
 //inserir lista servico injetor
 function inserirCalibInjServico(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Calibragem Injetor", botao)
 }
 
 function inserirTestInjServico(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Teste Injetor", botao)
 }
 
 function inserirLimpInjServico(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Limpeza Injetor", botao)
 }
 
 function inserirRetInjServico(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Ret/Colocar Injetor", botao)
 }
 
 //inserir lista pecas unidades
 function inserirRepExUnidade(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Reparo Externo Unidade", botao)
 }
 
 function inserirRepInUnidade(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Reparo Geral Unidade", botao)
 }
 //inserir lista servico unidades
 function inserirTestUnidade(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Teste Unidade", botao)
 }
 
 function inserirRevUnidade(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Revisao Unidade", botao)
 }
 
 function inserirLimpUnidade(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Limpezao Unidade", botao)
 }
 
 //inserir outras pecas
 function inserirCanPeca(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Caneta Injetora", botao)
 }
 
 function inserirAnCanPeca(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Anel Caneta Injetora", botao)
 }
 
 function inserirFiltroArPeca(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Filtro AR", botao)
 }
@@ -337,11 +299,9 @@ function inserirFiltroDiPeca(botao) {
     if(contFdiesel == 0){
         numFdiesel = window.prompt("Informe o numero do Filtro Diesel:").toUpperCase()
         contFdiesel = 1
-        botao.style.backgroundColor = "gray"
         radios(qtd)
         busca(qtd ," Filtro Diesel " + numFdiesel, botao)
     }else{
-        botao.style.backgroundColor = "gray"
         radios(qtd)
         busca(qtd ," Filtro Diesel " + numFdiesel, botao)
         contFdiesel = 0
@@ -352,11 +312,9 @@ function inserirFiltroSePeca(botao) {
     if(contFsed == 0){
         numFsed = window.prompt("Informe o numero do Filtro Sedimentador:").toUpperCase()
         contFsed = 1
-        botao.style.backgroundColor = "gray"
         radios(qtd)
         busca(qtd ," Filtro Sedimentador " + numFsed, botao)
     }else{
-        botao.style.backgroundColor = "gray"
         radios(qtd)
         busca(qtd ," Filtro Sedimentador " + numFsed, botao)
         contFsed = 0
@@ -364,60 +322,49 @@ function inserirFiltroSePeca(botao) {
 }
 
 function inserirValSegRailPeca(botao) {
-    botao.style.backgroundColor = "gray"
     radios(qtd)
     busca(qtd, " Valvula Seguranca RAIL", botao)
 }
 
 function inserir20DieselPeca(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("20 Litros Diesel", botao)
 }
 
 function inserir40DieselPeca(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("40 Litros Diesel", botao)
 }
 
 //inserir outros servico na lista servico
 
 function inserirScanServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Diagnostico SCANNER", botao)
 }
 
 function inserirLimpRailServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Limpeza Tubo Rail", botao)
 }
 
 function inserirLimpTanqServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Limpeza Tanque", botao)
 }
 
 function inserirLimpCatServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Limpeza Catalizador", botao)
 }
 
 function inserirDesAdmisServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Descarbonizacao de Admissao", botao)
 }
 
 function inserirTestInterServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Teste Intercooler", botao)
 }
 
 function inserirRegValvServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Regulagem de Valvulas", botao)
 }
 
 function inserirRevChiServico(botao) {
-    botao.style.backgroundColor = "gray"
     buscaServico("Revisao Chicote", botao)
 }
 
